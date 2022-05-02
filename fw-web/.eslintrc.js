106b3256764b9b6c8b87cf1ca56b91aa3cfb2bd9
Igor Trentini <igortrentini.2004@gmail.com>
2022-05-02 12:54:01 -0300

    feat: Configurado eslint com prettier

---
+++ fw-web/.eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:prettier/recommended'
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['react', 'react-hooks', '@typescript-eslint, prettier'],
+	rules: {
+		indent: ['error', 'tab'],
+		'linebreak-style': ['error', 'unix'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'always'],
+		'import/no-unresolved': 'error',
+	},
+	settings: {
+		'import/resolver': {
+			typescript: {},
+		},
+	},
+};
